bbdd3df0d7db1ada76f1b5fd0403930773f2102e
karthikeya <karthikeyakotegar@gmail.com>
2023-04-30 07:22:45 +0530

    navabr menus and logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import { useRouter } from "next/router";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  console.log(router);
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span
+        className={`h-[1px] inline-block bg-dark abolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      {/* menus */}
+      <nav className="flex">
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
+      </nav>
+
+      {/* logo */}
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+
+      {/* social icons */}
+      <nav>
+        <Link href="/" target={"_blank"}>
+          K
+        </Link>
+        <Link href="/" target={"_blank"}>
+          K
+        </Link>
+        <Link href="/" target={"_blank"}>
+          K
+        </Link>
+        <Link href="/" target={"_blank"}>
+          K
+        </Link>
+        <Link href="/" target={"_blank"}>
+          K
+        </Link>
+        <Link href="/" target={"_blank"}>
+          K
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+import Head from "next/head";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Karthikeya Portfolio</title>
+        <meta name="description" content="Welcome to Techers World" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <Navbar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }

0fbbc910b709188b028e14e7a55a46b926948ee6
karthikeya <karthikeyakotegar@gmail.com>
2023-05-16 11:40:16 +0530

    articles dark

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -67,7 +67,7 @@ const MovingImg = ({ img, title, link }) => {
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-black rounded-2xl">
+    <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-black rounded-2xl dark:border-light dark:bg-dark">
       <div className="absolute top-0  -right-3 -z-10 w-[101%] h-[103%]  bg-dark rounded-[2rem] rounded-br-3xl " />
       <Link
         href={link}
@@ -91,7 +91,7 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
 
       <p className="text-sm mb-2">{summary}</p>
 
-      <span className="text-primary font-semibold">{time}</span>
+      <span className="text-primary dark:text-primaryDark font-semibold">{time}</span>
     </li>
   );
 };
@@ -110,10 +110,12 @@ const Article = ({ img, title, date, link }) => {
         },
       }}
       viewport={{ once: true }}
-      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4"
+      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light"
     >
       <MovingImg title={title} img={img} link={link} />
-      <span className="text-primary font-semibold pl-4">{date}</span>
+      <span className="text-primary font-semibold pl-4 dark:text-primaryDark">
+        {date}
+      </span>
     </motion.li>
   );
 };
@@ -126,7 +128,7 @@ const articles = () => {
         <meta name="description" content="karthikeya's Articles" />
       </Head>
 
-      <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
+      <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light">
         <Layout className="pt-16">
           <AnimatedText text="Words Can Change The World!" className="mb-16" />
 
